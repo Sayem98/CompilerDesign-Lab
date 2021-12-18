18190ac8d97e4da98102dba46dd9d92b5d3ffe41
Sayem98 <sayemabedin.bd@gmail.com>
2021-12-18 14:06:11 +0600

    Problem 5

---
+++ problem5.cpp
@@ -0,0 +1,75 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int len,i,j,cnt=0,temp,temp2;
+    string s;
+    cout<<"Enter a string:"<<endl;
+    freopen("input5.txt","r",stdin);
+    getline(cin, s);
+    len=s.length();
+    if(len == 0){
+        cout<<"Enter a valid string."<<endl;
+        return 0;
+    }
+    if(s[0]=='*'||s[0]=='/'||s[len-1]=='+'||s[len-1]=='-'||s[len-1]=='*'||s[len-1]=='/'||s[len-1]=='(')
+    {
+        cout<<"Invalid"<<endl;
+        return 0;
+    }
+    for(i=0;i<len;i++)
+    {
+        if(s[i]=='+'||s[i]=='-'||s[i]=='*'||s[i]=='/')
+        {
+            if(s[i+1]=='+'||s[i+1]=='-'||s[i+1]=='*'||s[i+1]=='/'|| s[i+1]==')')
+            {
+                cout<<"Invalid"<<endl;
+                return 0;
+
+            }
+        }
+        if(s[i]==')')
+        {
+            temp=i;
+        }
+        else if(s[i]=='('&&i+2<len)
+        {
+          temp2=i;
+          if(s[i+1]==')')
+              {
+              cout<<"Invalid"<<endl;
+              return 0;
+
+              }
+            else if(s[i+1]=='*'||s[i+1]=='/'){
+                cout<<"invalid"<<endl;
+                return 0;
+            }
+            else{
+                for(j=i;j<len;j++)
+                {
+                    if(s[j]==')')
+                    {
+                        cnt++;
+                    }
+                }
+            if(cnt==0)
+            {
+                cout<<"Invalid"<<endl;
+                return 0;
+
+            }
+        }
+
+        }
+    }
+    if(temp<temp2)
+    {
+        cout<<"Invalid"<<endl;
+        return 0;
+    }
+    cout<<"valid"<<endl;
+    return 0;
+
+
+}
